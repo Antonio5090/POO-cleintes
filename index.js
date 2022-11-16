02cb267bb38ea3b40fbaf2262c8859bdfc17ac4c
José Antonio Aguilera Sánchez <anthony5090@outlook.com>
2022-11-16 09:59:20 -0600

    "Quitando archivos para una mejor organización"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {Cliente} from "./cliente.js";
-import { CuentaAhorro } from "./CuentaAhorro.js";
-import { CuentaCorriente } from "./CuentaCorriente.js";
+import { CuentaAhorro } from "./Cuentas/CuentaAhorro.js";
+import { CuentaCorriente } from "./Cuentas/CuentaCorriente.js";
+import { CuentaNomina } from "./Cuentas/CuentaNomina.js";
 
 const cliente1 = new Cliente("Juan", "12345678", "12345678-9");
 const cliente2 = new Cliente("Pedro", "245648", "161819-6");
@@ -10,7 +11,14 @@ const cuentaPedro = new CuentaCorriente(cliente2, "2", "002");
 
 const cuentaAhorroJuan = new CuentaAhorro(cliente1, '9985', '001', 0);
 
-console.log(cuentaJuan);
+const cuentaNominaJuan = new CuentaNomina(cliente1, '9985', '001', 100);
+
+console.log(cuentaNominaJuan);
+cuentaNominaJuan.depositoEnCuenta(150);
+console.log(cuentaNominaJuan.consultarSaldo());
+cuentaNominaJuan.retirarDeCuenta(50);
+console.log(cuentaNominaJuan.consultarSaldo());
+/* console.log(cuentaJuan);
 cuentaJuan.depositoEnCuenta(150);
 console.log(cuentaJuan.consultarSaldo());
 cuentaJuan.retirarDeCuenta(70);
@@ -20,4 +28,4 @@ console.log(cuentaAhorroJuan);
 cuentaAhorroJuan.depositoEnCuenta(200);
 console.log(cuentaAhorroJuan.consultarSaldo());
 cuentaAhorroJuan.retirarDeCuenta(120);
-console.log(cuentaAhorroJuan.consultarSaldo());
+console.log(cuentaAhorroJuan.consultarSaldo()); */
